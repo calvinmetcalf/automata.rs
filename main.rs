9a6815154d49221abec4ad28b921dfdf0feb2746
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-13 10:32:01 -0800

    better command line

--- main.rs
+++ main.rs
@@ -6,5 +6,14 @@ fn main() {
     let code = from_str::<u8>(args[1]).unwrap_or(30u8);
     let vString = args[2];
     let mut value = 0u8;
-    println!("next value is: {}",compute(code,value));
+    if(vString[0] == 49){
+    	value += 4;
+    }
+    if(vString[1] == 49){
+    	value += 2;
+    }
+    if(vString[2] == 49){
+    	value += 1;
+    }
+    println!("{}",compute(code,value));
 }
